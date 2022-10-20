ad624a0e02a1e32c9ff4b3410a1b197a9ca29289
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2022-10-20 17:01:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = "  Bob  ";
+trim($name);
+
+
+$age = "Thirty five";
+if($age == "Thirty five"){
+    "35";
+ }
+
+$job = "plumber";
+
+$carMake = " Toyota";
+
+$carColour = "RED";
+strtolower($carColour);
+
+$car = ($carColour . $carMake);
+$happy = true;
+
+$time = "this many";
+if($time == "this many"){
+  "420 Hours";
+ }
+$Bob_retires = 65;
+
+
+echo"{$Name} is a {$job}. He drives a {$car} and works {$time} hours a day. He is {$age} years old and is {$Bob_retires} years from retirement. He is happy with his life.";
+
+?>
+
